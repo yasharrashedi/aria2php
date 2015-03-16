ecd5a3678301d49b4f69526576899f7e481f5899
Yashar Rashedi <info@rashedi.com>
2015-03-16 15:03:28 +0330

    changed config

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ session_start();
 define('HOST','localhost');
 define('PORT','6800');
 define('DOWNLOAD_PATH','/mnt/downloads/');
-define('GUI_USERNAME','radfa');
-define('GUI_PASSWORD','radfa');
+define('GUI_USERNAME','testuser');
+define('GUI_PASSWORD','secret');
 
