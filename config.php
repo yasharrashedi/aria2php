26dfc5cbb9972e0dd690300491b86a6cbc0a3f79
renwei <renwei.chen@icloud.com>
2017-03-25 15:38:25 +0800

    1

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('HOST','localhost');
 define('SECRET','secret');
 define('PORT','6800');
 define('DOWNLOAD_PATH','/mnt/downloads/');
-//define('GUI_USERNAME','GUEST');
-//define('GUI_PASSWORD','GUEST');
+define('GUI_USERNAME','GUEST');
+define('GUI_PASSWORD','GUEST');
